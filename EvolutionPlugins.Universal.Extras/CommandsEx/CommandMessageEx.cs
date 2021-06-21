3b9b632980bb6cca3e8533fd4277fdcb5d1e4dad
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-06-21 18:09:56 +0700

    Add CommandMessageEx

---
+++ EvolutionPlugins.Universal.Extras/CommandsEx/CommandMessageEx.cs
@@ -0,0 +1,30 @@
+﻿using EvolutionPlugins.Universal.Extras.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using OpenMod.API.Users;
+using OpenMod.Core.Commands;
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace EvolutionPlugins.Universal.Extras.CommandsEx
+{
+    public abstract class CommandMessageEx : Command
+    {
+        private readonly IMessageManager m_MessageManager;
+
+        public CommandMessageEx(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            m_MessageManager = serviceProvider.GetRequiredService<IMessageManager>();
+        }
+
+        public override Task PrintAsync(string message)
+        {
+            return m_MessageManager.PrintMessageAsync((IUser)Context.Actor, message);
+        }
+
+        public override Task PrintAsync(string message, Color color)
+        {
+            return m_MessageManager.PrintMessageAsync((IUser)Context.Actor, message, color);
+        }
+    }
+}
